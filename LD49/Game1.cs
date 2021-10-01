f05214cfa7636395073103440cd232b2620612e8
notexplosive <thenotexplosive@gmail.com>
2021-10-01 13:04:19 -0700

    Run Rider formatter

--- LD49/Game1.cs
+++ LD49/Game1.cs
@@ -1,13 +1,12 @@
 ﻿using Machina.Engine;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace LD49
 {
     public class Game1 : MachinaGame
     {
-        public Game1(string[] args) : base("LD49", args, new Point(1600, 900), new Point(1600, 900), ResizeBehavior.MaintainDesiredResolution)
+        public Game1(string[] args) : base("LD49", args, new Point(1600, 900), new Point(1600, 900),
+            ResizeBehavior.MaintainDesiredResolution)
         {
         }
 
--- LD49/Program.cs
+++ LD49/Program.cs
@@ -5,10 +5,12 @@ namespace LD49
     public static class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             using (var game = new Game1(args))
+            {
                 game.Run();
+            }
         }
     }
 }
