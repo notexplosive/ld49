44592f6252afd10ca631f8fb8da9d8701d14ec99
notexplosive <thenotexplosive@gmail.com>
2021-10-02 13:37:47 -0700

    Move to own file

---
+++ LD49/Data/MathOperator.cs
@@ -0,0 +1,35 @@
+﻿namespace LD49.Data
+{
+    public static class MathOperator
+    {
+        public static MathExpression Multiply(MathExpression left, MathExpression right)
+        {
+            return MultiplyMathExpression.Create(left, right);
+        }
+
+        public static MathExpression Negate(MathExpression value)
+        {
+            return NegateExpression.Create(value);
+        }
+
+        public static MathExpression Inverse(MathExpression value)
+        {
+            return InverseExpression.Create(value);
+        }
+
+        public static MathExpression Add(MathExpression left, MathExpression right)
+        {
+            return AddMathExpression.Create(left, right);
+        }
+
+        public static MathExpression Subtract(MathExpression left, MathExpression right)
+        {
+            return MathOperator.Add(left, MathOperator.Negate(right));
+        }
+
+        public static MathExpression Divide(MathExpression left, MathExpression right)
+        {
+            return MultiplyMathExpression.Create(left, MathOperator.Inverse(right));
+        }
+    }
+}
