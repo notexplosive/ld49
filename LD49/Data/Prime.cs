1b898e1c32d8a44b1f2544d28bb3e169ba64b0aa
notexplosive <thenotexplosive@gmail.com>
2021-10-02 14:51:43 -0700

    Make fields readonly

--- LD49/Data/Prime.cs
+++ LD49/Data/Prime.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace LD49.Data
 {
     public class Prime : Number
     {
         private static readonly Dictionary<int, Prime> All = new Dictionary<int, Prime>();
-        public static Prime Two = new Prime(2);
-        public static Prime Three = new Prime(3);
-        public static Prime Five = new Prime(5);
-        public static Prime Seven = new Prime(7);
-        public static Prime Eleven = new Prime(11);
-        public static Prime Thirteen = new Prime(13);
+        public static readonly Prime Two = new Prime(2);
+        public static readonly Prime Three = new Prime(3);
+        public static readonly Prime Five = new Prime(5);
+        public static readonly Prime Seven = new Prime(7);
+        public static readonly Prime Eleven = new Prime(11);
+        public static readonly Prime Thirteen = new Prime(13);
 
         private Prime(int value) : base(value)
         {
