7ffcd78b466f69009557b60570368daa2502775c
notexplosive <thenotexplosive@gmail.com>
2021-10-02 15:10:21 -0700

    Named variables

---
+++ LD49/Data/NamedVariable.cs
@@ -0,0 +1,26 @@
+﻿namespace LD49.Data
+{
+    public class NamedVariable : MathExpression
+    {
+        private static int totalIndex;
+        private readonly int index;
+        private readonly string name;
+
+        public static readonly NamedVariable X = new NamedVariable("X");
+        public static readonly NamedVariable Y = new NamedVariable("Y");
+        public static readonly NamedVariable Z = new NamedVariable("Z");
+        
+        private NamedVariable(string name)
+        {
+            this.index = NamedVariable.totalIndex++;
+            this.name = name;
+        }
+
+        public override int UnderlyingValue => int.MinValue + this.index;
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ TestLD49/VariableTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LD49.Data;
+using Microsoft.VisualBasic.CompilerServices;
+using Xunit;
+
+namespace TestLD49
+{
+    public class VariableTests
+    {
+        [Fact]
+        public void named_variable_comes_first_in_expression()
+        {
+            MathOperator
+                .Add(MathOperator.Add(Prime.Eleven, MathOperator.Add(NamedVariable.X, Prime.Seven)),
+                    MathOperator.Add(Prime.Thirteen, Prime.Three)).ToString().Should().Be("(X + 3 + 7 + 11 + 13)");
+        }
+
+        [Fact]
+        public void x_over_x_is_one()
+        {
+            MathOperator.Divide(NamedVariable.X, NamedVariable.X).Should().Be(One.Instance);
+        }
+        
+        [Fact]
+        public void x_times_inverse_x_is_x()
+        {
+            MathOperator.Multiply(NamedVariable.X, MathOperator.Inverse(NamedVariable.X)).Should().Be(One.Instance);
+        }
+
+        [Fact]
+        public void x_minus_x_is_zero()
+        {
+            MathOperator.Subtract(NamedVariable.X, NamedVariable.X).Should().Be(Zero.Instance);
+        }
+    }
+}
