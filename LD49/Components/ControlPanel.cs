48b54465e189830d9a198e304131272340fdeaf4
notexplosive <thenotexplosive@gmail.com>
2021-10-03 15:30:54 -0700

    Extract component

--- LD49/Components/ControlPanel.cs
+++ LD49/Components/ControlPanel.cs
@@ -76,31 +76,9 @@ namespace LD49.Components
         {
             new Hoverable(primeButtonActor);
             new TooltipProvider(primeButtonActor, number.ToString());
-            var renderer = new ExpressionRenderer(primeButtonActor, false, number);
+            new ExpressionRenderer(primeButtonActor, false, number);
 
-            new Clickable(primeButtonActor);
-            var draggable = new Draggable(primeButtonActor);
-
-            void DragStart(Vector2 mousePos, Vector2 delta)
-            {
-                Reckoning.DragHand.PickUp(renderer);
-                Reckoning.DragHand.actor.Visible = true;
-            }
-
-            void DragEnd(Vector2 mousePos, Vector2 delta)
-            {
-                Reckoning.DragHand.actor.Visible = false;
-                Reckoning.DragHand.Drop(mousePos);
-            }
-
-            draggable.DragStart += DragStart;
-            draggable.DragEnd += DragEnd;
-
-            new CallbackOnDestroy(primeButtonActor, () =>
-            {
-                draggable.DragStart -= DragStart;
-                draggable.DragEnd -= DragEnd;
-            });
+            new DragNumber(primeButtonActor);
         }
 
         private void BuildNavigationButton(string tooltip, Action callback)
---
+++ LD49/Components/DragNumber.cs
@@ -0,0 +1,50 @@
+﻿using Machina.Components;
+using Machina.Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LD49.Components
+{
+    public class DragNumber : BaseComponent
+    {
+        private readonly Draggable draggable;
+        private readonly ExpressionRenderer renderer;
+
+        public DragNumber(Actor actor) : base(actor)
+        {
+            this.renderer = RequireComponent<ExpressionRenderer>();
+
+            new Clickable(actor);
+            this.draggable = new Draggable(actor);
+
+            this.draggable.DragStart += DragStart;
+            this.draggable.DragEnd += DragEnd;
+        }
+
+        public override void OnDeleteFinished()
+        {
+            this.draggable.DragStart -= DragStart;
+            this.draggable.DragEnd -= DragEnd;
+        }
+
+        private void DragStart(Vector2 mousePos, Vector2 delta)
+        {
+            Reckoning.DragHand.PickUp(this.renderer);
+            Reckoning.DragHand.actor.Visible = true;
+        }
+
+        private void DragEnd(Vector2 mousePos, Vector2 delta)
+        {
+            Reckoning.DragHand.actor.Visible = false;
+            Reckoning.DragHand.Drop(mousePos);
+        }
+
+        public override void Update(float dt)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
