ca206ce8bcb3f5e34b4709fd8ad47fcae564c82d
notexplosive <thenotexplosive@gmail.com>
2021-10-04 12:43:51 -0700

    modify release script

--- release-build.bat
+++ release-build.bat
@@ -1,3 +1,3 @@
 neato monogame-release-build .\LD49 upload-temp-folder
-neato publish-itch upload-temp-folder notexplosive reckoning main
+neato publish-itch upload-temp-folder notexplosive expressions main
 rmdir /Q /S upload-temp-folder
